4bff0d8131e76c59c76c5ea8a419d4d9daba6411
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-07 00:58:38 +0800

    优惠券信息怎么读取？

--- weixin/application/module/user/free_score.act.php
+++ weixin/application/module/user/free_score.act.php
@@ -20,6 +20,16 @@ class UserFree_score extends Action
     public function on_free_score()
     {
         $rows = $this->db->where('user_id', $this->user_id)->result('free_get_score');
+
+        //读取oracle中优惠券信息
+        if ($rows) {
+            foreach ($rows as $row) {
+                $codes[] = $row['code'];
+            }
+            //TODO oracle数据库配置
+            // $oracle_dbh = $this->dbh = new PDO($host, $user, $password, $options);
+        }
+
         $this->view->assign('rows', $rows);
         $this->view->display('user/free_score.html');
     }
