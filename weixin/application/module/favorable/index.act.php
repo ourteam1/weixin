81611a616e8f74750c2691d7c73deab947113e7c
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-07 22:48:21 +0800

    优惠卷

--- weixin/application/module/favorable/index.act.php
+++ weixin/application/module/favorable/index.act.php
@@ -14,30 +14,23 @@ class FavorableIndex extends Action {
     }
 
     function on_index() {
-
-        $arr = array(
-            array(
-                'code' => '3201506117212',
-                'name' => 'M折扣-大0617伊利奶粉送80元',
-                'score'=> 10,
-                'start_time' => '2015-03-01',
-                'end_time' => '2015-09-30',
-            ),
-            array(
-                'code' => '3201501196874',
-                'name' => 'R折扣-营0120物美会员满200送5',
-                'score'=> 15,
-                'start_time' => '2015-03-01',
-                'end_time' => '2015-08-30',
-            ),
-            array(
-                'code' => '3201506117213',
-                'name' => 'R折扣-营0120物美会员购买燕京200送5',
-                'score'=> 20,
-                'start_time' => '2015-04-01',
-                'end_time' => '2015-11-31',
-            ),
-        );
+        $activityUrl = "http://42.62.73.239:8080/CloudServer/wi.do?method=GetDiscount_WX";
+        $activityXmlObj = file_get_contents($activityUrl);
+        $activityXmlObj = iconv('gbk', 'utf-8', $activityXmlObj);
+        $res = @simplexml_load_string($activityXmlObj,NULL,LIBXML_NOCDATA);
+        $activityArr = json_decode(json_encode($res),true);
+        
+        $arr = array();
+        foreach ($activityArr['discount'] as $activity) {
+           
+            $arr[] = array(
+                'code'       => $activity['hdbh'],
+                'name'       => $activity['hdnr'],
+                'score'      => $activity['gold'],
+                'start_time' => $activity['kssj'],
+                'end_time'   => $activity['jssj'],
+            );
+        }       
         
         // 领取的优惠卷
         $activityNums = array();
