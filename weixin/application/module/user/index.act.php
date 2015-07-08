845519912cb796f86217c2d84704f9812c65af63
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-09 01:04:29 +0800

    界面bug修改

--- weixin/application/module/user/index.act.php
+++ weixin/application/module/user/index.act.php
@@ -28,7 +28,9 @@ class UserIndex extends Action
         }
 
         $date = date('Y-m-d H:i:s');
-        $rows = $this->db->select('user_id')->where('user_id', $this->user_id)->where('start_time', '<=', $date)->where('end_time', '>=', $date)->result('favorable');
+        // $rows = $this->db->select('user_id')->where('user_id', $this->user_id)->where('start_time', '<=', $date)->where('end_time', '>=', $date)->result('favorable');
+        $sth  = $this->db->query("select * from ms_favorable left join ms_activity on ms_favorable.activity_code=ms_activity.activity_code where start_time<='$date' and end_time>='$date'");
+        $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
         $this->view->assign('favorable_count', count($rows));
 
         $this->view->display('user/index.html');
