fa004121bc139bfc0a74c0ccaedaa05ef71eec93
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-10 07:36:32 +0800

    缺少单引号

--- weixin/application/module/code/gamelist.act.php
+++ weixin/application/module/code/gamelist.act.php
@@ -16,7 +16,7 @@ class CodeGamelist extends Action {
         $games = array(
             array('name'=>'摇一摇得金币', 'fold'=>'yao'),
             array('name'=>'快来一起捡钱玩', 'fold'=>'money'),
-			array('name'=>'抽奖赢金币', 'fold'=>'choujiang),
+            array('name'=>'抽奖赢金币', 'fold'=>'choujiang'),
         );
         $this->view->assign('games', $games);
         $this->view->display('code/gamelist.html');
