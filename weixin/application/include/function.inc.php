8297448f9777d758d3cf07b8f172f9d498cdba1f
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-13 21:38:13 +0800

    字符串截取

--- weixin/application/include/function.inc.php
+++ weixin/application/include/function.inc.php
@@ -124,6 +124,37 @@ if (!function_exists('die_json')) {
 }
 
 /**
+ * 字符串截取
+ */
+if (!function_exists('substr_cut')) {
+    function substr_cut($str, $sublen, $etc = '...') {
+        if (strlen($str) <= $sublen) {
+            $rStr = $str;
+        } else {
+            $I = 0;
+            while ($I < $sublen) {
+                $StringTMP = substr($str, $I, 1);
+                if (ord($StringTMP) >= 224) {
+                    $StringTMP = substr($str, $I, 3);
+                    $I = $I + 3;
+                } elseif (ord($StringTMP) >= 192) {
+                    $StringTMP = substr($str, $I, 2);
+                    $I = $I + 2;
+                } else {
+                    $I = $I + 1;
+                }
+                $StringLast[] = $StringTMP;
+            }
+
+            $rStr = implode('', $StringLast) . $etc;
+        }
+
+        return $rStr;
+    }
+
+}
+
+/**
  * 当系统出现严重错误时
  */
 if (!function_exists('_shutdown')) {
