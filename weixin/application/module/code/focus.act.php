b3a8d941d37032b94b566926b5d7eaa53d5b54e6
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-28 18:15:02 +0800

    金币不增加bug

--- weixin/application/module/code/focus.act.php
+++ weixin/application/module/code/focus.act.php
@@ -1,19 +1,24 @@
 <?php
 
-if (!defined('IN_MSAPP')) exit('Access Deny!');
+if (!defined('IN_MSAPP')) {
+    exit('Access Deny!');
+}
 
 /**
- * 
+ *
  * http://host/weixin/test.php/code/enter
  */
-class CodeFocus extends Action {
+class CodeFocus extends Action
+{
 
-    function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->wxauth();
     }
 
-    function on_focus($code = '') {
+    public function on_focus($code = '')
+    {
         if (empty($code)) {
             die_json(array('error_code' => 1, 'error' => '参数错误'));
         }
@@ -28,20 +33,23 @@ class CodeFocus extends Action {
         $this->db->trans_start();
 
         $freeData = array(
-            'code' => $code,
-            'user_id' => $this->user_id,
-            'create_time' => date('Y-m-d H:i:s')
+            'code'        => $code,
+            'user_id'     => $this->user_id,
+            'create_time' => date('Y-m-d H:i:s'),
         );
         $res = $this->db->insert("focus_user", $freeData);
-        if (FALSE === $res) {
-             $this->db->trans_rollback(); // 回滚事务
+        if (false === $res) {
+            $this->db->trans_rollback(); // 回滚事务
             die_json(array('error_code' => 3, 'error' => '关注失败'));
         }
-        
+
         $userinfo = $this->db->where('user_id', $this->user_id)->row('user');
-        $score = !empty($focusInfo['score']) ? $focusInfo['score'] : 0;        
+
+        $score = $this->db->where('focus_id', $code)->column('focus', 'score');
+        $score = !empty($score) ? $score : 0;
+
         $data = array(
-            'score' =>  $userinfo['score'] + $score,
+            'score'       => $userinfo['score'] + $score,
             'modify_time' => date('Y-m-d H:i:s'),
         );
         $res = $this->db->where('user_id', $this->user_id)->update('user', $data);
@@ -52,18 +60,18 @@ class CodeFocus extends Action {
 
         // 记录账户变动记录
         $data = array(
-            'user_id' => $this->user_id,
-            'action' => 'user.score.add',
-            'action_name' => '增加金币' . $score ,
-            'amount' => $score,
+            'user_id'     => $this->user_id,
+            'action'      => 'user.score.add',
+            'action_name' => '增加金币' . $score,
+            'amount'      => $score,
             'create_time' => date('Y-m-d H:i:s'),
         );
         $this->db->insert('user_account', $data);
-        
+
         // 提交事务
         $this->db->trans_commit();
-        
-        die_json(array('message'=>'关注成功'));
+
+        die_json(array('message' => '关注成功'));
     }
 
 }
