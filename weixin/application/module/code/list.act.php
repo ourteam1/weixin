79f70650ac199cb085caf21a95db1039c6a02424
ourteam1 <ourteam1@users.noreply.github.com>
2015-07-28 17:16:56 +0800

    关注bug修改

--- weixin/application/module/code/list.act.php
+++ weixin/application/module/code/list.act.php
@@ -1,32 +1,37 @@
 <?php
 
-if (!defined('IN_MSAPP')) exit('Access Deny!');
+if (!defined('IN_MSAPP')) {
+    exit('Access Deny!');
+}
 
 /**
- * 
+ *
  * http://host/weixin/test.php/code/list
  */
-class CodeList extends Action {
+class CodeList extends Action
+{
 
-    function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->wxauth();
     }
 
-    function on_list() {
+    public function on_list()
+    {
         // 获取关注列表
         $focusList = $this->db->where('status', 1)->order_by('sort', 'asc')->result('focus');
         foreach ($focusList as $k => $i) {
-            $i['icon'] = !empty($i['icon']) ? IMAGED . $i['icon'] : '';
+            $i['icon']     = !empty($i['icon']) ? IMAGED . $i['icon'] : '';
             $focusList[$k] = $i;
         }
 
-        $freeCodes = array();
-        $freeGetList = $this->db->where('user_id', $this->user_id)->result('free_get_score');
-        foreach($freeGetList as $freeCode) {
+        $freeCodes   = array();
+        $freeGetList = $this->db->where('user_id', $this->user_id)->result('focus_user');
+        foreach ($freeGetList as $freeCode) {
             $freeCodes[] = $freeCode['code'];
         }
-        
+
         $this->view->assign('freeCodes', $freeCodes);
         $this->view->assign('focusList', $focusList);
         $this->view->display('code/list.html');
